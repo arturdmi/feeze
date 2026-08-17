41d55ad29eadb16e771cfdbe578108059eb146d7
Artur Dmitriienko <arturdmi@users.noreply.github.com>
2026-08-17 14:14:26 +0200

    move dependency installation into a composite action

---
+++ .github/actions/install-deps/install-deps.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# This file is part of the feeze scheduling analysis tool.
+#
+# This code is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License, version 3,
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License, version 3,
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+
+# -----------------------------------------------------------------------
+#
+#  Copyright (c) 2025, Tokiwa Software GmbH, Germany
+#
+#  Source of install-deps.sh
+#
+#  This installs the toolchain needed to build and package feeze on a
+#  GitHub Actions runner
+#
+#
+# -----------------------------------------------------------------------
+sudo apt-get update
+sudo apt-get install openjdk-25-jdk-headless libgc1 libgc-dev
+sudo apt-get install libelf-dev
+sudo apt-get install linux-tools-`uname -r`
+sudo apt-get install pandoc
+
+echo "JAVA_HOME=/usr/lib/jvm/java-25-openjdk-amd64" >> $GITHUB_ENV
+echo "/usr/lib/jvm/java-25-openjdk-amd64/bin" >> $GITHUB_PATH
