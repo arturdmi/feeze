53bca5d4ab2fae546ad3daf0b73f5b1859cc8bdc
Simon von Hackewitz <simon.von.hackewitz@tokiwa.software>
2026-09-04 17:48:11 +0200

    use existing KeyListener to toggle fullscreen with F11

--- src/java/dev/feeze/ControlFrame.java
+++ src/java/dev/feeze/ControlFrame.java
@@ -344,7 +344,19 @@ public class ControlFrame extends JFrame
 
         var kl = new KeyListener()
           {
-            @Override public void keyPressed(KeyEvent key) { }
+            @Override public void keyPressed(KeyEvent key)
+            {
+              if (key.getKeyCode() == KeyEvent.VK_F11)
+                {
+                  // toggle fullscreen
+                  var device = getGraphicsConfiguration().getDevice();
+                  device.setFullScreenWindow(
+                    device.getFullScreenWindow() == ControlFrame.this
+                      ? null
+                      : ControlFrame.this
+                  );
+                }
+            }
             @Override public void keyReleased(KeyEvent key) { }
             @Override public void keyTyped(KeyEvent key) {
               if (key.getKeyChar() == 'w' - 0x60)
